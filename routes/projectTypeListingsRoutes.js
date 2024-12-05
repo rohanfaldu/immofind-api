782bdd54bc5b8acb5ddc7f43d41765def8553bc4
bhavuka <bhavujaviya@gmail.com>
2024-12-05 16:55:18 +0530

    propertyType and propertyType  and projectTypeList update delete API

--- routes/projectTypeListingsRoutes.js
+++ routes/projectTypeListingsRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import {getProjectTypeList, createProjectTypeListing} from '../controllers/projectTypeListingsController.js';
+import {getProjectTypeList, createProjectTypeListing,updateProjectTypeListing,deleteProjectTypeListing} from '../controllers/projectTypeListingsController.js';
 const router = express.Router();
 
 router.post('/', getProjectTypeList);
 router.post('/create', createProjectTypeListing);
+router.put('/:ID', updateProjectTypeListing);
+router.delete('/:ID', deleteProjectTypeListing);
 
 
 export default router;
--- routes/propertyTypeListingsRoutes.js
+++ routes/propertyTypeListingsRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import { getAllPropertyTypeListings, createPropertyTypeListing} from '../controllers/propertyTypeListingsController.js';
+import { getAllPropertyTypeListings, createPropertyTypeListing,updatePropertyTypeListing,deletePropertyTypeListing} from '../controllers/propertyTypeListingsController.js';
 import passport from 'passport';
 const router = express.Router();
 
 router.post('/', getAllPropertyTypeListings);
 router.post('/create', createPropertyTypeListing);
 // router.post('/:id', getPropertyTypeListingById);
-// router.put('/:id', updatePropertyTypeListing);
-// router.delete('/:id', deletePropertyTypeListing);
+router.put('/:id', updatePropertyTypeListing);
+router.delete('/:id', deletePropertyTypeListing);
 
 export default router;
