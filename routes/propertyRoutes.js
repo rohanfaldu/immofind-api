861a1878fe880a615ead68a41cd995ad5209c6d8
deven <devensureja07@gmail.com>
2025-03-20 12:04:44 +0530

    Demo spaces

--- routes/propertyRoutes.js
+++ routes/propertyRoutes.js
@@ -25,7 +25,7 @@ router.post('/get-liked-property-user', authorize, getUserLikedData);
 router.post('/get-viewed-property-user', authorize, getUserViewedData);
 router.post('/get-commented-property-user', authorize, getUserCommentedData);
 router.post('/user-like-property', authorize, getUserLikeProperty);
-router.post('/user-view-property', authorize, getUserViewProperty);
+router.post('/user-view-property', authorize, getUserViewProperty); 
 router.post('/user-activity', authorize, setUserActivity); 
 // router.delete('/:propertyId/like', authorize, unlikeProperty);
 export default router;
